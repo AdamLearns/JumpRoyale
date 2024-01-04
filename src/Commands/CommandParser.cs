d78098af72d2384cd122d475da7d358ae70f5663
darkstoorm <b.sieprawski@op.pl>
2024-01-04 12:46:13 +0100

    Remove the discard check and TODO
    There already is a check in the Arena (OnMessage) that will only go through if the message part matches the desired command name or alias, so there is no need to check for the arguments length.
    
    The discard check is not needed anyway, because the argument parsing will fall back to default value or will be simply ignored on higher count

--- src/Commands/CommandParser.cs
+++ src/Commands/CommandParser.cs
@@ -11,18 +11,7 @@ public abstract class CommandParser
     {
         _arguments = chatMessage.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-        /// TODO: test if it's actually possible to receive an empty message from Twitch, because
-        /// previously this had a check for <1 Arguments Length, but Twitch blocks empty
-        /// message requests, although there could be extensions preventing that
-
         _name = _arguments[0].ToLower();
-
-        // Check the last part. If it's not a number, then just reject it entirely.
-        // This way, people can get around the duplicate-message problem on Twitch.
-        if (!int.TryParse(_arguments[^1], out _))
-        {
-            _arguments = _arguments.Take(_arguments.Length - 1).ToArray();
-        }
     }
 
     public abstract bool IsValid();
