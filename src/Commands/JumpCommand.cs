b2d797e6bd5aef5ef01cc195ded16a7cd84fe8db
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-01-06 20:27:43 +0100

    Rewrite JumpCommand
    The `BaseCommand` has been dropped, because the parameter parse now occurs directly in the caught chat message event.
    
    The abstraction turned out to be unnecessary, since there is no complex logic related to the commands currently implemented in the game.

--- src/Commands/JumpCommand.cs
+++ src/Commands/JumpCommand.cs
@@ -1,81 +1,69 @@
 using System;
 using System.Collections.Generic;
 
-public class JumpCommand : BaseChatCommand
+internal class JumpCommand
 {
-    private int _power = 100;
+    private int _power;
 
     /// <summary>
-    /// Angle extracted from the chat command. If there was no Angle provided in the arguments,
-    /// it will default to `0` and be overridden internally, based on the command alias.
-    /// This value is clamped between `-90` and `90`.
-    ///
-    /// Note that this may get interpreted as power, which is why we don't clamp it here.
+    /// Specifies at what angle the player should jump, where 0 points up and the angles count
+    /// Clockwise. Positive: right, negative: left.
     /// </summary>
-    public int Angle { get; set; } = 0;
+    /// <remarks>
+    /// This value is clamped between -90 and 90. See <see cref="AdjustAngle"/> for external
+    /// clamp exceptions
+    /// </remarks>
+    public int Angle { get; private set; } = 0;
 
     /// <summary>
-    /// Jump power in form of "_percentage_" the players can apply as an argument in the chat
+    /// Jump power in form of "percentage" the players can apply as an argument in the chat
     /// message. This value is clamped between `1` and `100`.
     /// </summary>
     public int Power
     {
         get { return _power; }
-        set { _power = Math.Clamp(value, 1, 100); }
+        private set { _power = Math.Clamp(value, 1, 100); }
     }
 
-    private readonly List<string> _allowedCommandAliases =
-        new() { "jump", "j", "l", "u", "r", "ul", "ur" };
+    private readonly List<string> _fixedAngleDirections = new() { "u", "ur", "ul" };
 
-    private readonly List<string> _fixedAngleCommands = new() { "u", "ur", "ul" };
-
-    public JumpCommand(string chatMessage)
-        : base(chatMessage)
+    public JumpCommand(string direction, int? angle, int? power)
     {
-        string angleFromCommand = arguments.Length > 1 ? arguments[1] : "0";
-        string powerFromCommand = arguments.Length > 2 ? arguments[2] : "100";
+        Angle = angle ?? 0;
+        Power = power ?? 100;
 
-        if (int.TryParse(angleFromCommand, out int angle))
-        {
-            Angle = angle;
-        }
-
-        if (int.TryParse(powerFromCommand, out int power))
-        {
-            Power = power;
-        }
-
-        AdjustAngle();
+        AdjustAngle(direction);
     }
 
-    public override bool IsValid()
+    private void AdjustAngle(string direction)
     {
-        return _allowedCommandAliases.Contains(Name);
-    }
-
-    private void AdjustAngle()
-    {
-        /// The command aliases are exceptions, where the angle is fixed, but we should be able
-        /// to at least modify the power of those jump and we will do this by assigning the
-        /// first argument as Power, so we don't want to force the Angle to be present
-        if (_fixedAngleCommands.Contains(Name))
+        /// There are commands, which imply in which direction we would like to jump without
+        /// providing an angle. We don't want to explicitly specify an angle to jump
+        /// straight up, which is inconvenient when avoiding duplicate messages
+        if (_fixedAngleDirections.Contains(direction))
         {
-            /// The default Angle value is always 0, so we will only assign the Angle if the user
-            /// has actually provided something, otherwise jump at maximum power (100). At
-            /// this point we can't have clamped angle, because we will lose 10 power
+            /// Players may still want to specify their own Power when jumping at fixed-angle, so
+            /// we can get around this by interpreting the first parameter as Power without
+            /// having to specify Angle, then Power to read the second parameter
             Power = Angle != 0 ? Angle : 100;
         }
 
+        /// Important: the reason why this value is not clamped in the property itself is that we
+        /// have commands, which have implied angle and only accept Power as parameter, which
+        /// we read from Angle and we can't clamp this value, otherwise we lose 10 Power
         Angle = Math.Clamp(Angle, -90, 90);
 
-        Angle = Name switch
+        Angle = direction switch
         {
             "jump" or "j" or "r" => Angle + 90,
             "l" => 90 - Angle,
             "u" => 90,
-            "ul" => 45,
-            "ur" => 135,
+            "ul" => 30,
+            "ur" => 120,
             _ => 90 /* fall back to upwards */
         };
+
+        // TODO: Consider testing other fixed-angle commands: Dash Left / Right (dl/dr)
+        // Angle: 80 or 75
     }
 }
