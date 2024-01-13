2a5e10c456ba013d6e74583105695c30909bcaaf
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-01-13 21:28:45 +0100

    Apply code style rules

--- src/Commands/JumpCommand.cs
+++ src/Commands/JumpCommand.cs
@@ -1,39 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 internal class JumpCommand
 {
+    private readonly List<string> _fixedAngleDirections = new() { "u", "ll", "lll", "jj", "rr", "jjj", "rrr" };
+
     private int _power;
 
-    /// <summary>
-    /// Specifies at what angle the player should jump, where 0 points up and the angles count
-    /// Clockwise. Positive: right, negative: left.
-    /// </summary>
-    /// <remarks>
-    /// This value is clamped between -90 and 90. See <see cref="AdjustAngle"/> for external
-    /// clamp exceptions
-    /// </remarks>
-    public int Angle { get; private set; } = 0;
+    public JumpCommand(string direction, int? angle, int? power)
+    {
+        Angle = angle ?? 0;
+        Power = power ?? 100;
+
+        AdjustAngle(direction);
+    }
 
     /// <summary>
-    /// Jump power in form of "percentage" the players can apply as an argument in the chat
+    /// Gets the clamped angle from user inputs. Angle of 0 degrees points up and the angles count Clockwise.
+    /// Positive: right, negative: left.
+    /// </summary>
+    /// <remarks>
+    /// See <see cref="AdjustAngle"/> for details on external clamp of this value.
+    /// </remarks>
+    public int Angle { get; private set; }
+
+    /// <summary>
+    /// Gets the current jump power in form of "percentage" after the players can apply as an argument in the chat
     /// message. This value is clamped between `1` and `100`.
     /// </summary>
     public int Power
     {
         get { return _power; }
         private set { _power = Math.Clamp(value, 1, 100); }
-    }
-
-    private readonly List<string> _fixedAngleDirections = new() { "u", "ll", "lll", "jj", "rr", "jjj", "rrr" };
-
-    internal JumpCommand(string direction, int? angle, int? power)
-    {
-        Angle = angle ?? 0;
-        Power = power ?? 100;
-
-        AdjustAngle(direction);
     }
 
     private void AdjustAngle(string direction)
@@ -54,32 +52,27 @@ internal class JumpCommand
         // we read from Angle and we can't clamp this value, otherwise we lose 10 Power
         Angle = Math.Clamp(Angle, -90, 90);
 
-        Angle = AngleFromDirectionCommand(direction, Angle);
-    }
-
-    public static int AngleFromDirectionCommand(string direction, int? angleFromUser, bool shouldThrow = false)
-    {
-        int angle = angleFromUser ?? 0;
-
-        // Predefined set of angles for available jump commands. Experimental commands were
-        // defined just as repeated letters for easier typing. The pattern matching has
-        // been changed to allow typos and garbage and still allow changing the angle
-        return direction switch
+        Angle = direction switch
         {
+            // Predefined set of angles for available jump commands. Experimental commands were
+            // defined just as repeated letters for easier typing. The pattern matching has
+            // been changed to allow typos and garbage and still allow changing the angle
             // Warning: experimental commands - rrr/rr/lll/ll :)
+            // TODO: Change the format of double/triple char commands, because they can collide
+            // with intentionally typo-ed commands for message duplication evasion
             string when direction.StartsWith("rrr") || direction.StartsWith("jjj") => 150,
             string when direction.StartsWith("rr") || direction.StartsWith("jj") => 120,
             string when direction.StartsWith("lll") => 30,
             string when direction.StartsWith("ll") => 60,
-            string when direction.StartsWith("j") || direction.StartsWith("r") => angle + 90,
-            string when direction.StartsWith("l") => 90 - angle,
-            string when direction.StartsWith("u") => 90,
-            _ => !shouldThrow ? 90 : throw new Exception($"Unhandled direction command: ({direction})")
+            string when direction.StartsWith('j') || direction.StartsWith('r') => Angle + 90,
+            string when direction.StartsWith('l') => 90 - Angle,
+            string when direction.StartsWith('u') => 90,
+            _ => 90
         };
     }
 
     private bool MatchesFixedAngleAlias(string direction)
     {
-        return _fixedAngleDirections.Any(alias => direction.StartsWith(alias));
+        return _fixedAngleDirections.Exists(alias => direction.StartsWith(alias));
     }
 }
