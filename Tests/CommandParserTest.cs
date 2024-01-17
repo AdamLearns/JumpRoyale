077a29f1dad19e198a7607adf383fb31d0c441f0
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-01-17 20:17:20 +0100

    Add a placeholder test

---
+++ Tests/CommandParserTest.cs
@@ -0,0 +1,11 @@
+namespace Tests;
+
+public class CommandParserTest
+{
+    [Test]
+    public void PlaceholderTest()
+    {
+        // This is just a placeholder until old tests get moved into NUnit
+        Assert.Pass();
+    }
+}
