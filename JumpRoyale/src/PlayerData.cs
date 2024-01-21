4275e7caeaaa1f1d7b30ed57640850d09e9c90dd
darkstoorm <b.sieprawski@op.pl>
2024-01-21 18:54:43 +0100

    Implement NameColor property

--- JumpRoyale/src/PlayerData.cs
+++ JumpRoyale/src/PlayerData.cs
@@ -1,6 +1,6 @@
 // Note to contributors: do not rename anything in this class without updating the corresponding JSON data on disk.
 
-public class PlayerData(string glowColor, int characterChoice)
+public class PlayerData(string glowColor, int characterChoice, string nameColor)
 {
     public int Num1stPlaceWins { get; set; }
 
@@ -18,6 +18,8 @@ public class PlayerData(string glowColor, int characterChoice)
     public string GlowColor { get; set; } = glowColor;
 
     public int CharacterChoice { get; set; } = characterChoice;
+
+    public string NameColor { get; set; } = nameColor;
 
     // Note that this can change between games since you can change your name on
     // Twitch, so this is just for convenience of looking at the JSON file and
