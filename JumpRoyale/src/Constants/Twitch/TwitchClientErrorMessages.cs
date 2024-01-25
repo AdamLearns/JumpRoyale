58bf65a33e5495b36e5bfe1c73e65b785ca117a8
darkstoorm <b.sieprawski@op.pl>
2024-01-25 14:17:46 +0100

    Add `Channel Id` to constants

--- JumpRoyale/src/Constants/Twitch/TwitchClientErrorMessages.cs
+++ JumpRoyale/src/Constants/Twitch/TwitchClientErrorMessages.cs
@@ -4,5 +4,8 @@ public static partial class TwitchConstants
         $"No access token found. Please run `dotnet user-secrets set {TwitchAccessTokenKey} <your access token>`";
 
     public const string MissingChannelNameError =
-        $"Channel not found. Please run `dotnet user-secrets set {TwitchChannelNameKey} <your twitch channel>`";
+        $"Channel not found. Please run `dotnet user-secrets set {TwitchChannelNameKey} <your twitch channel name>`";
+
+    public const string MissingChannelIdError =
+        $"Channel Id not found. Please run `dotnet user-secrets set {TwitchChannelIdKey} <your twitch channel id>`";
 }
--- JumpRoyale/src/Constants/Twitch/TwitchSecretsConfigurationKeys.cs
+++ JumpRoyale/src/Constants/Twitch/TwitchSecretsConfigurationKeys.cs
@@ -9,4 +9,9 @@ public static partial class TwitchConstants
     /// User-secrets Channel Name index for IConfigurationRoot.
     /// </summary>
     public const string TwitchChannelNameKey = "twitch_channel_name";
+
+    /// <summary>
+    /// User-secrets Channel Id index for IConfigurationRoot.
+    /// </summary>
+    public const string TwitchChannelIdKey = "twitch_channel_id";
 }
