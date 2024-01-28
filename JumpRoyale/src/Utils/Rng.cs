c4d6bd42bd2bb7c98cd2c140ab7ded43f0e10128
darkstoorm <b.sieprawski@op.pl>
2024-01-28 15:28:06 +0100

    Revert to normal Random

--- JumpRoyale/src/Utils/Rng.cs
+++ JumpRoyale/src/Utils/Rng.cs
@@ -1,27 +1,15 @@
 using System;
-using System.Security.Cryptography;
 
 public static class Rng
 {
-    private static readonly RandomNumberGenerator _rng = RandomNumberGenerator.Create();
+    private static readonly Random _rng = new();
 
     /// <summary>
     /// Returns a random integer from specified range (right-inclusive).
     /// </summary>
     public static int IntRange(int min, int max)
     {
-        uint range = (uint)(max - min + 1);
-        uint number = GetNumber() % range;
-
-        return (int)(number + min);
-    }
-
-    /// <summary>
-    /// Returns a random float between 0 and 1.
-    /// </summary>
-    public static float RandomFloat()
-    {
-        return (float)GetNumber() / uint.MaxValue;
+        return _rng.Next(min, max);
     }
 
     /// <summary>
@@ -29,16 +17,6 @@ public static class Rng
     /// </summary>
     public static string RandomHex()
     {
-        return new Godot.Color(RandomFloat(), RandomFloat(), RandomFloat(), 1).ToHtml(false);
-    }
-
-    private static uint GetNumber()
-    {
-        byte[] bytes = new byte[4];
-
-        // Fill the bytes with random sequence
-        _rng.GetBytes(bytes);
-
-        return BitConverter.ToUInt32(bytes, 0);
+        return new Godot.Color(_rng.NextSingle(), _rng.NextSingle(), _rng.NextSingle(), 1).ToHtml(false);
     }
 }
