9863141eadffab5b320f9ef9171d1cb003a2cc8a
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-01-29 20:29:35 +0100

    Add Json constants/helpers

---
+++ JumpRoyale/src/Constants/Messages/JsonDeserializationMessages.cs
@@ -0,0 +1,5 @@
+public static partial class JsonDeserializationConstants
+{
+    public const string JsonDeserializationError =
+        $"No records returned from the json string. Make sure the JSON string appears to be valid and contains data.";
+}
