1f28664cad30eaddbead7bec24923f8c17f67643
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-01-29 18:58:49 +0100

    Revert StyleCop rule violation

--- JumpRoyale/src/AllPlayerData.cs
+++ JumpRoyale/src/AllPlayerData.cs
@@ -3,9 +3,6 @@ using System.Text.Json.Serialization;
 
 public class AllPlayerData
 {
-    /// <summary>
-    /// Dictionary of Players
-    /// </summary>
 #pragma warning disable CA2227 // Collection properties should be read only
     [JsonPropertyName("players")]
     public Dictionary<string, PlayerData> Players { get; set; } = [];
