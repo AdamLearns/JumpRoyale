e2a0e346bb406b5cd46818d0dfc7e9291a0d7084
darkstoorm <b.sieprawski@op.pl>
2024-01-30 03:11:51 +0100

    Add more Json tests

--- Tests/Game/PlayerStatsTests.cs
+++ Tests/Game/PlayerStatsTests.cs
@@ -1,11 +1,13 @@
+using System.Text.Json;
+
 namespace Game;
 
 [TestFixture]
 public class PlayerStatsTests
 {
-    private readonly AllPlayerData _allPlayerData = new();
     private readonly string _testFile = "data.json";
-    private PlayerStats? _playerStats;
+    private PlayerStats _playerStats;
+    private PlayerData _fakePlayer;
 
     /// <summary>
     /// Location of the Test directory, where the data will be read/written to.
@@ -26,8 +28,8 @@ public class PlayerStatsTests
     [SetUp]
     public void SetUp()
     {
-        // Remove any previously loaded data
-        _allPlayerData.Players.Clear();
+        _playerStats = new(FullPath);
+        _fakePlayer = new(Rng.RandomHex(), Rng.IntRange(1, 18), Rng.RandomHex()) { UserId = "1", };
 
         // Create the directory on fresh deploy or after project cleanup
         if (!Directory.Exists(TestPath))
@@ -55,9 +57,79 @@ public class PlayerStatsTests
     {
         Assert.DoesNotThrow(() =>
         {
-            _playerStats = new(FullPath);
-
-            Assert.That(_playerStats.LoadPlayerData(), Is.False);
+            _playerStats.LoadPlayerData();
         });
+    }
+
+    /// <summary>
+    /// This test makes sure that we don't throw exceptions on allowed conditions of the json data being "null" or empty
+    /// object, because the player data collection is still initialized as empty and later populated at runtime, when
+    /// serializing players.
+    /// </summary>
+    [Test]
+    public void DoesNotThrowWhenObjectIsEmptyOrNull()
+    {
+        bool state = false;
+
+        Assert.DoesNotThrow(() =>
+        {
+            // Literal "null" can't be assigned to the AllPlayerData, so this should return early with false
+            File.WriteAllText(FullPath, "null");
+
+            state = _playerStats.LoadPlayerData();
+        });
+
+        Assert.That(state, Is.False);
+        state = false;
+
+        Assert.DoesNotThrow(() =>
+        {
+            // Having an empty object
+            File.WriteAllText(FullPath, "{}");
+
+            state = _playerStats.LoadPlayerData();
+        });
+
+        Assert.That(state, Is.True);
+    }
+
+    [Test]
+    public void CanThrowExceptionOnMalformedJson()
+    {
+        File.WriteAllText(FullPath, "{fdf,}}");
+
+        Assert.Throws<JsonException>(() =>
+        {
+            _playerStats.LoadPlayerData();
+        });
+    }
+
+    /// <summary>
+    /// This test makes sure that the player can be loaded properly after being saved into the file. Note: this does not
+    /// need to be deeply equal, we are only comparing the json data.
+    /// </summary>
+    [Test]
+    public void CanSerializePlayerThenDeserialize()
+    {
+        // We should start with an empty dictionary, so we will add a fake player, serialize him and then attempt to
+        // read him from the file, then make sure the same data is loaded
+        _playerStats.UpdatePlayerById(_fakePlayer.UserId, _fakePlayer);
+        _playerStats.SaveAllPlayers();
+
+        // Start on a fresh stats instance
+        _playerStats = new(FullPath);
+        _playerStats.LoadPlayerData();
+
+        PlayerData? playerFromFile = _playerStats.GetPlayerById(_fakePlayer.UserId);
+
+        if (playerFromFile is null)
+        {
+            Assert.Fail();
+        }
+
+        string fakeSerialized = JsonSerializer.Serialize(_fakePlayer);
+        string playerFromFileSerialized = JsonSerializer.Serialize(playerFromFile);
+
+        Assert.That(fakeSerialized, Is.EqualTo(playerFromFileSerialized));
     }
 }
