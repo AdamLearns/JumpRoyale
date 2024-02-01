119adbd9517bcfc2835b77e029ef8ea2ab99915a
darkstoorm <b.sieprawski@op.pl>
2024-02-01 02:36:22 +0100

    Add new helper exception

--- JumpRoyale/src/Constants/Messages/PlayerStatsMessages.cs
+++ JumpRoyale/src/Constants/Messages/PlayerStatsMessages.cs
@@ -6,5 +6,8 @@ public static partial class PlayerStatsMessages
     public const string MissingPlayerStatsPath =
         $"Path to the stats file was not initialized. Make sure you set the path to stats file before loading players.";
 
+    public const string NonExistentPlayer =
+        "The player you are trying to update does not exist in the dictionary. Did you mean to store instead?";
+
     public const string NullPlayerData = "No PlayerData provided.";
 }
