8f39ced3b3e96bee2e3ac2e815d1119f8c580113
darkstoorm <b.sieprawski@op.pl>
2024-08-18 20:04:36 +0200

    Add new test sprites
    If they won't look good on stream, we will most likely remove them

--- JumpRoyale/src/Arena.cs
+++ JumpRoyale/src/Arena.cs
@@ -143,7 +143,13 @@ public partial class Arena : Node2D
     private void SetBackground()
     {
         Sprite2D background = GetNode<Sprite2D>("Background");
-        string[] colors = ["Blue", "Brown", "Gray", "Green", "Pink", "Purple", "Yellow"];
+
+        // Note: old backgrounds, which still are present in the project
+        // Note2: the old backgrounds use "modulate = Color(1, 1, 1, 0.25098)" in the editor, because they are bright,
+        // the modulate has been removed for new textures and were made darker through image editing. Revert this change
+        // if the old backgrounds are used again.
+        // string[] colors = ["Blue", "Brown", "Gray", "Green", "Pink", "Purple", "Yellow"];
+        string[] colors = ["Bricks", "Tiles", "TilesAlt", "BrickWallAlt", "BrickWall", "BricksAlt"];
         string color = colors[Rng.IntRange(0, colors.Length - 1)];
 
         background.Texture = ResourceLoader.Load<Texture2D>($"res://assets/sprites/backgrounds/{color}.png");
