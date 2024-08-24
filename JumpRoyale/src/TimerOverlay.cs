89de836ae4195f051c5b2aa324bed04d60019f25
Bartosz Sieprawski <b.sieprawski@op.pl>
2024-08-24 16:51:45 +0200

    Refactor + change Modulation
    This change mostly extracts magic numbers and caches the components.
    
    This change also introduces new colors for timer overlay, which only
    differ in Alpha channel being slightly reduced. The reasoning for this
    is that when players stay behind the timer, they are completely covered
    and you can't see if anyone is standing behind it when they are at the
    top.

--- JumpRoyale/src/TimerOverlay.cs
+++ JumpRoyale/src/TimerOverlay.cs
@@ -6,12 +6,34 @@ public partial class TimerOverlay : VFlowContainer
     private const string SpriteName = "Background";
     private const string TimerNodeName = SpriteName + "/Timer";
 
+    /// <summary>
+    /// Determines when the timer will start flashing red.
+    /// </summary>
+    private const int FinalCountdown = 15;
+
+    private const float ColorAlpha = 0.75f;
+
     private static readonly int GameLength = GameOverlay.GameLength;
 
     private int _timerSeconds = GameLength;
 
+    private Color _defaultColor = new(1, 1, 1, ColorAlpha);
+    private Color _flashColor = new(1, 0, 0, ColorAlpha);
+
+    private Label _timerLabel = null!;
+
+    private Sprite2D _sprite = null!;
+
     [Signal]
     public delegate void TimerDoneEventHandler();
+
+    public override void _Ready()
+    {
+        _timerLabel = GetNode<Label>(TimerNodeName);
+        _sprite = GetNode<Sprite2D>(SpriteName);
+
+        _sprite.Modulate = _defaultColor;
+    }
 
     public void Init()
     {
@@ -27,6 +49,7 @@ public partial class TimerOverlay : VFlowContainer
         if (_timerSeconds <= 0)
         {
             EmitSignal(SignalName.TimerDone);
+
             return;
         }
 
@@ -40,14 +63,15 @@ public partial class TimerOverlay : VFlowContainer
         int seconds = _timerSeconds % 60;
         int minutes = _timerSeconds / 60;
 
-        GetNode<Label>(TimerNodeName).Text = $"{minutes}:{seconds:00}";
-        Sprite2D sprite = GetNode<Sprite2D>(SpriteName);
-        if (_timerSeconds > 15)
+        _timerLabel.Text = $"{minutes}:{seconds:00}";
+
+        if (_timerSeconds > FinalCountdown)
         {
-            sprite.Modulate = Colors.White;
+            _sprite.Modulate = _defaultColor;
+
             return;
         }
 
-        sprite.Modulate = seconds % 2 == 0 ? Colors.White : Colors.Red;
+        _sprite.Modulate = seconds % 2 == 0 ? _defaultColor : _flashColor;
     }
 }
