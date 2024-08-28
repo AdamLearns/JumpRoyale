2a0430050938069e86b1d7219ddc0ac41dca9e15
darkstoorm <b.sieprawski@op.pl>
2024-08-28 12:36:08 +0200

    Resolve conflict from `gfx-update`

--- JumpRoyale/src/ArenaBuilder.cs
+++ JumpRoyale/src/ArenaBuilder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 public class ArenaBuilder(TileMap tileMap)
@@ -5,20 +6,31 @@ public class ArenaBuilder(TileMap tileMap)
     /// <summary>
     /// Defines at what Y in tiles the object type should change. For reference, the screen is 63 tiles tall on a 1080p
     /// screen and Y goes negative upwards. Important note: the current arena is placed one screen below the default
-    /// position, so the actual next screen is located at Y=0. The estimate average max height the players reach
+    /// position, so the actual next screen is located at Y=0. The estimated average max height the players reach
     /// during the game is around 3500px (actually -155 height in the game), so the last texture change should be
     /// "almost" at the estimated max height. Despite the screen being 63 tiles tall, we change the object type every 50
     /// tiles to give players a chance to see all the textures. With 63~ tiles per change, the last change occurs at
     /// 4000px, which will barely be seen by players.
     /// </summary>
-    private readonly int[] _objectTypeChangeHeights = [0, -50, -100, -150];
+    private readonly KeyValuePair<int, GameObjectType>[] _objectHeightsAndTypes =
+    [
+        new(0, GameObjectType.Concrete),
+        new(-50, GameObjectType.Terracotta),
+        new(-100, GameObjectType.Brick),
+        new(-150, GameObjectType.Gold)
+    ];
+
     private readonly TileMap _tileMap = tileMap;
 
+    /// <summary>
+    /// Draws a horizontal platform at given position. The smallest platform, for visual purposes, will always be 2
+    /// tiles wide, because we have to draw the left and the right side. The Width only determines the extension of the
+    /// platform, which is the middle part and when we say "Draw Platform of Width 1", we mean a platform that is 1 tile
+    /// wide + the left and right side.
+    /// </summary>
+    /// <param name="width">Amount of additional tiles to draw - the middle part of the platform.</param>
     public void DrawPlatform(int x, int y, int width)
     {
-        // Note: the smallest platform, for visual purposes, will always be 2 tiles wide, because we have to draw the
-        // left and the right side. The Width only determines the extension of the platform, which is the middle part
-        // and when we say "Draw Platform of Width 1", we mean a platform that is 1 tile wide + the left and right side.
         BaseObject gameObject = GameObject.Get(GetObjectType(y));
         int endX = x + width + 1;
 
@@ -53,15 +65,17 @@ public class ArenaBuilder(TileMap tileMap)
     /// <param name="y">Current height in the arena. Mind the negative Y.</param>
     private GameObjectType GetObjectType(int y)
     {
-        return y switch
+        for (int i = _objectHeightsAndTypes.Length - 1; i >= 0; i--)
         {
-            // Note: can't use a simple "< _objectTypeChangeHeights[3]", because it requires a constant value
-            var value when value < _objectTypeChangeHeights[3] => GameObjectType.Gold,
-            var value when value < _objectTypeChangeHeights[2] => GameObjectType.Brick,
-            var value when value < _objectTypeChangeHeights[1] => GameObjectType.Terracotta,
-            var value when value < _objectTypeChangeHeights[0] => GameObjectType.Concrete,
-            _ => GameObjectType.Stone // Default object will always be drawn first - the brown stone object
-        };
+            KeyValuePair<int, GameObjectType> pair = _objectHeightsAndTypes[i];
+
+            if (y < pair.Key)
+            {
+                return pair.Value;
+            }
+        }
+
+        return GameObjectType.Stone;
     }
 
     private void DrawAt(int x, int y, Vector2I obj)
--- JumpRoyale/src/GameObjects/GameObject.cs
+++ JumpRoyale/src/GameObjects/GameObject.cs
@@ -5,11 +5,36 @@ public static class GameObject
     private static readonly Dictionary<GameObjectType, BaseObject> _gameObjects =
         new()
         {
-            [GameObjectType.Stone] = new Stone(),
-            [GameObjectType.Concrete] = new Concrete(),
-            [GameObjectType.Terracotta] = new Terracotta(),
-            [GameObjectType.Brick] = new Brick(),
-            [GameObjectType.Gold] = new Gold(),
+            [GameObjectType.Stone] = new BaseObject(
+                GameObjectAtlasCoords.StoneBlock,
+                GameObjectAtlasCoords.StonePlatformLeft,
+                GameObjectAtlasCoords.StonePlatformMiddle,
+                GameObjectAtlasCoords.StonePlatformRight
+            ),
+            [GameObjectType.Concrete] = new BaseObject(
+                GameObjectAtlasCoords.ConcreteBlock,
+                GameObjectAtlasCoords.ConcretePlatformLeft,
+                GameObjectAtlasCoords.ConcretePlatformMiddle,
+                GameObjectAtlasCoords.ConcretePlatformRight
+            ),
+            [GameObjectType.Terracotta] = new BaseObject(
+                GameObjectAtlasCoords.TerracottaBlock,
+                GameObjectAtlasCoords.TerracottaPlatformLeft,
+                GameObjectAtlasCoords.TerracottaPlatformMiddle,
+                GameObjectAtlasCoords.TerracottaPlatformRight
+            ),
+            [GameObjectType.Brick] = new BaseObject(
+                GameObjectAtlasCoords.BrickBlock,
+                GameObjectAtlasCoords.BrickPlatformLeft,
+                GameObjectAtlasCoords.BrickPlatformMiddle,
+                GameObjectAtlasCoords.BrickPlatformRight
+            ),
+            [GameObjectType.Gold] = new BaseObject(
+                GameObjectAtlasCoords.GoldBlock,
+                GameObjectAtlasCoords.GoldPlatformLeft,
+                GameObjectAtlasCoords.GoldPlatformMiddle,
+                GameObjectAtlasCoords.GoldPlatformRight
+            ),
         };
 
     public static BaseObject Get(GameObjectType type) => _gameObjects[type];
